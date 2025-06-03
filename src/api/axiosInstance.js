9eed6b196fb918b1249d67584fd304a8b390c67d
Kimgyuill <rlarbdlf222@gmail.com>
2025-06-03 16:37:29 +0900

    feat: 배포 설정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:8080',  // 로컬 Spring Boot 백엔드 주소
-  baseURL: 'http://54.180.157.93:8080',  // 배포 Spring Boot 백엔드 주소
-  timeout: 5000,                     // 요청 시간 초과 설정 (5초)
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default axiosInstance;
